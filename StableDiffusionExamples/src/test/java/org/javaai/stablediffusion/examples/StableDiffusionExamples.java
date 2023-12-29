5aef8427076a0a50d45e59c74c62d03f44445e4d
javaai.org <javaai.org@gmail.com>
2023-12-29 23:14:22 +0800

    Load dynamic link library example.

---
+++ StableDiffusionExamples/src/test/java/org/javaai/stablediffusion/examples/StableDiffusionExamples.java
@@ -0,0 +1,39 @@
+package org.javaai.stablediffusion.examples;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.javaai.stablediffusion.api.StableDiffusionLoader;
+import org.junit.jupiter.api.Test;
+
+public class StableDiffusionExamples {
+	
+	
+	
+	
+	
+	
+	public static void loadLibrary() {
+		StableDiffusionLoader.loadShared();
+	}
+	
+	
+	@Test
+	void testLoadLibrary() throws Exception {
+		
+		loadLibrary();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
