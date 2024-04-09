d5d063a973ac366eda2959016d5949469e895967
javaai.org <javaai.org@gmail.com>
2024-04-09 23:00:03 +0800

    v1.0.4-RELESE, update jni libs to fix String format errors on macos platform.

--- StableDiffusionConsole/src/main/java/org/javaai/stablediffusion/console/Main.java
+++ StableDiffusionConsole/src/main/java/org/javaai/stablediffusion/console/Main.java
@@ -1,7 +1,9 @@
 package org.javaai.stablediffusion.console;
 
+import org.javaai.stablediffusion.api.SDLogCallback;
 import org.javaai.stablediffusion.api.StableDiffusion;
 import org.javaai.stablediffusion.api.StableDiffusionLoader;
+import org.javaai.stablediffusion.api.Util;
 
 public class Main {
 	
@@ -11,6 +13,18 @@ public class Main {
 		StableDiffusionLoader.loadShared();
 		
 		StableDiffusion sd = new StableDiffusion();
+		Util.setSDLogCallback(new SDLogCallback() {
+			
+			@Override
+			public void onSDLogCallback(int SDLogLevel, String file, int line, String log) {
+				System.out.println(log);
+			}
+		});
+		
+		boolean loadFromFile = sd.loadFromFile("/Users/u/Desktop/models/sd-v1-4.ckpt", null, null, null);
+		
+		System.out.println("load:" + loadFromFile);
+		sd.txt2img("test");
 		
 		
 		sd.close();
